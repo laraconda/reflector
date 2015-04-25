4fca1535f4e86d84210666480881f9780976ac33
César Alejandro Lara <laralacesar@gmail.com>
2015-04-25 14:11:46 -0500

    Chars that its mirror is also common bug fixed

--- criteria.py
+++ criteria.py
@@ -4,3 +4,4 @@
 no_need_to_change_base_chars = ['i', 'l', 'm', 'n', 'o', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '	', ':', '_', '#', ',', '.', '\n', '\'', 'T']
 original_base_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'p', 'q', 'r', 's', 't', '(', ')']
 mirrored_base_chars = ['α', 'd', 'ɔ', 'b', 'ɘ', 'ʇ', 'ǫ', 'ʜ', 'Ⴑ', 'ʞ', 'q', 'p', 'ɿ', 'ƨ', 'ƚ', ')', '(']
+char_that_its_mirror_is_common = ['(', ')', '{', '}', '[', ']', 'b', 'd', 'p', 'q']
--- untitled.py
+++ untitled.py
@@ -1,6 +1,8 @@
 import sublime, sublime_plugin
 from sublime import Region
 from criteria import no_need_to_change_base_chars, original_base_chars, mirrored_base_chars
+from criteria import char_that_its_mirror_is_common
+
 
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -22,9 +24,14 @@ def invert_lines(my_view, edit):
 	for region_line in lines:
 		line = my_view.substr(region_line)
 		flipped_line = line[::-1]
-		for char_to_mirror in flipped_line:
+		for i, char_to_mirror in enumerate(flipped_line):
 			mirror_char = get_mirror_char(char_to_mirror).decode('utf-8')
-			flipped_line = flipped_line.replace(char_to_mirror, mirror_char)
+			if char_to_mirror in char_that_its_mirror_is_common:
+				flipped_line_list = list (flipped_line)
+				flipped_line_list[i] = mirror_char
+				flipped_line = ''.join(flipped_line_list)
+			else:
+				flipped_line = flipped_line.replace(char_to_mirror, mirror_char)
 		my_view.replace(edit, region_line, flipped_line)
 	return None
 
@@ -33,14 +40,9 @@ def get_mirror_char(char_to_mirror):
 	if char_to_mirror in no_need_to_change_base_chars:
 		return char_to_mirror
 	elif char_to_mirror in original_base_chars:
-		char = mirrored_base_chars[original_base_chars.index(char_to_mirror)]
+		index = original_base_chars.index(char_to_mirror)
+		char = mirrored_base_chars[index]
 		return char
 	return char_to_mirror
-
-"""
-def open_criteria_file():
-	criteria = open('criteria.txt', 'r')
-	return criteria
-"""
 # view.run_command('example')
 	
